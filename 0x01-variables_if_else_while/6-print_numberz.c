8d1a37caea32cbce35225c4f9a722a78bc1e465e
“mallam-ali” <gana.aliyu2020@uniabuja.edu.ng>
2022-10-17 14:48:29 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+	char *str = "0123456789";
+
+	for (num = 0 ; num <= 9 ; num++)
+	{
+		putchar(str[num]);
+	}
+	putchar('\n');
+	return (0);
+}
