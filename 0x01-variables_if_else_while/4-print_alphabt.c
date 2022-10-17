64962b564b15e5dd3e8ecd2f843916e3362a87f6
“mallam-ali” <gana.aliyu2020@uniabuja.edu.ng>
2022-10-17 15:11:27 -0700

    latest commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch, new_line;
+
+
+
+	new_line = '\n';
+	for (ch = 'a' ; ch <= 'z' ; )
+	{
+		if (ch == 'e' || ch == 'q')
+		ch++;
+		else
+		{
+			putchar(ch);
+			ch++;
+		}
+	}
+	putchar(new_line);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+	char new_line;
+
+	new_line = '\n';
+	for (num = 0 ; num <= 9 ; num++)
+	{
+		printf("%d", num);
+	}
+	putchar(new_line);
+	return (0);
+}
