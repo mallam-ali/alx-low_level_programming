e9d8ac353d8868d40445b668a87bf065606d7bb7
“mallam-ali” <gana.aliyu2020@uniabuja.edu.ng>
2022-10-17 12:21:14 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,23 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
-/** main - Entry point
- *
+/**
+ * main - Entry point
+ * The functuion will output the size of in 32 bits environment
+ * or the type size of in 64bits environment
+ * compile it in 64 bits with no warning
+ * compile it in 32 bits with warning
  * Return: Always 0 (success)
  *
  */
 int main(void)
-
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float e;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
-
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 
 }
