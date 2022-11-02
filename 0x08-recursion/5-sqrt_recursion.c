70e435645e6c48be5a319b84a667365b583bd09a
“mallam-ali” <gana.aliyu2020@uniabuja.edu.ng>
2022-11-02 02:28:34 -0700

    correct

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,7 +18,6 @@ int power_operation(int n, int c)
 	return (0 + power_operation(n, c + 1));
 }
 /**
- *
  *  _sqrt_recursion - returns the natural square root of a number.
  *   @n: input number.
  *   Return: natural square root.
